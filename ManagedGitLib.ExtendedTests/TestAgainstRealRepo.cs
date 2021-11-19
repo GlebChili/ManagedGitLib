bdd56e60881417e90b1ee2a27a5339644170ade8
Gleb Krasilich <gleb.chili@gmail.com>
2021-11-19 11:49:35 +0300

    Added test against real mono/mono repo

--- ManagedGitLib.ExtendedTests/TestAgainstRealRepo.cs
+++ ManagedGitLib.ExtendedTests/TestAgainstRealRepo.cs
@@ -1,13 +1,60 @@
+using System;
+using System.IO;
 using Xunit;
+using LibGit2Sharp;
+using System.Diagnostics;
 
 namespace ManagedGitLib.ExtendedTests
 {
-    public class TestAgainstRealRepo
+    public class MonoRepoProvider : IDisposable
     {
-        [Fact]
-        public void Test1()
-        {
+        readonly DirectoryInfo repoDirectory;
+        readonly Repository repo;
 
+        public DirectoryInfo RepoDirectory => repoDirectory;
+        public Repository Repo => repo;
+
+        public MonoRepoProvider()
+        {
+            repoDirectory = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString()));
+
+            using Process gitProcess = new Process();
+            gitProcess.StartInfo.WorkingDirectory = repoDirectory.FullName;
+            gitProcess.StartInfo.CreateNoWindow = true;
+            gitProcess.StartInfo.FileName = "git";
+            gitProcess.StartInfo.ArgumentList.Add("clone");
+            gitProcess.StartInfo.ArgumentList.Add("https://github.com/mono/mono.git");
+            gitProcess.StartInfo.ArgumentList.Add(".");
+            gitProcess.Start();
+            gitProcess.WaitForExit();
+
+            repo = new Repository(repoDirectory.FullName);
+        }
+
+        public void Dispose()
+        {
+            repo.Dispose();
+
+            repoDirectory.SetFilesAttributesToNormal();
+            repoDirectory.Delete(true);
+        }
+    }
+
+    public class TestAgainstRealRepo : IClassFixture<MonoRepoProvider>
+    {
+        readonly MonoRepoProvider repoProvider;
+
+        public TestAgainstRealRepo(MonoRepoProvider repoProvider)
+        {
+            this.repoProvider = repoProvider;
+        }
+
+        [Fact]
+        public void GetCommitById1()
+        {
+            using GitRepository? repo = GitRepository.Create(repoProvider.RepoDirectory.FullName);
+
+            Assert.NotNull(repo);
         }
     }
 }
---
+++ ManagedGitLib.ExtendedTests/TestUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ManagedGitLib.ExtendedTests
+{
+    public class TestUtils
+    {
+    }
+
+    public static class HelperExtensions
+    {
+        public static void SetFilesAttributesToNormal(this DirectoryInfo directory)
+        {
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                File.SetAttributes(file.FullName, FileAttributes.Normal);
+            }
+
+            foreach (DirectoryInfo subdirectory in directory.GetDirectories())
+            {
+                subdirectory.SetFilesAttributesToNormal();
+            }
+        }
+    }
+}
